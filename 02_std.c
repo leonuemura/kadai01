53d166ee256156c429c872f33b461a6ff325df94
leonuemura <106325619+leonuemura@users.noreply.github.com>
2022-07-17 13:24:00 +0900

    Create 02_std.c

---
+++ 02_std.c
@@ -0,0 +1,18 @@
+#include "watalab.h"
+
+#define BUFSIZE 1024
+
+int main(int argc, char** argv)
+{
+  int ret;
+  char buf[BUFSIZE];
+  
+  ret = read(0, buf, BUFSIZE);
+  printf("read %d bytes\r\n", ret);
+  
+  ret = write(1, buf, ret);
+  printf("write %d bytes\r\n", ret);
+  
+  return 0;
+}
+  
