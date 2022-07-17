adea31b060c15e397d03dc5659b8c286aaa94f78
leonuemura <106325619+leonuemura@users.noreply.github.com>
2022-07-17 13:17:49 +0900

    Create 01_arg.c

---
+++ 01_arg.c
@@ -0,0 +1,12 @@
+#include "watalab.h"
+
+int main(int argc, char** argv)
+{
+  int i;
+  printf("The number of arguments is %d\n", argc);
+  for(i = 0; i < argc,; i++){
+    printf("%d: %s\r\n", i, argv[i]);
+  }
+  
+  return 0;
+}
